6c0e2c0b2e77c4dd2f937eb201770aae325881c4
Lujan <wasabisoundandmusic@gmail.com>
2025-11-26 19:11:27 -0300

    Actualizacion del __init__ con todas las clases

--- clases/__init__.py
+++ clases/__init__.py
@@ -1,2 +1,6 @@
 from clases.ruleta import Ruleta
 from clases.usuario import Usuario
+from clases.casillas import Casillas
+from clases.casillas import CasillaLose
+from clases.casillas import CasillaMorir
+from clases.casillas import CasillaWin
