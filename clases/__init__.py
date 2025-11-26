2345daed3f40e3e9fb82a0a491f28bb44d42b3d6
Lujan <wasabisoundandmusic@gmail.com>
2025-11-26 18:09:01 -0300

    Creacion de la vaiable con la que se generará la lista e instanciacion del Random

---
+++ clases/__init__.py
@@ -0,0 +1,1 @@
+from clases.ruleta import Ruleta
---
+++ clases/ruleta.py
@@ -0,0 +1,14 @@
+
+import random
+#from clases.casillas import Casillas | mas adelante voy a necesitar intanciar la clase
+
+class Ruleta(): #Clase con la que se va a crear el objeto Ruleta luego
+
+    def __init__(self):
+        self.__lista_de_casillas: list[Casillas] = [] #Aca encapsulamos en una lista las casillas
+
+
+
+    
+
+    
