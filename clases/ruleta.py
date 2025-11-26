340ebd71e2316daf22ddda9c1a141227b163f7c5
Lujan <wasabisoundandmusic@gmail.com>
2025-11-26 18:33:04 -0300

    Creación del metodo de seleccion de casilla y finalizacion del codigo de la clase Casilla

--- clases/ruleta.py
+++ clases/ruleta.py
@@ -1,6 +1,6 @@
 
 import random
-#from clases.casillas import Casillas | mas adelante voy a necesitar intanciar la clase
+from clases.casillas import Casillas 
 
 class Ruleta(): #Clase con la que se va a crear el objeto Ruleta luego
 
@@ -13,6 +13,10 @@ class Ruleta(): #Clase con la que se va a crear el objeto Ruleta luego
     def _ordenar_casillas(self) -> None: #Aca utilizamos el random para mezclar las casillas de manera azárica
         random.shuffle(self.__lista_de_casillas)
 
-
+    def detenerse(self) -> Casillas: #Esta funció devuelve casilla, al detenerse printea que casilla le salió al jugador
+        print("La ruleta se detiene y no sabemos donde...\n*Suenan los redoblantes*")
+        self._ordenar_casillas()  # Llama al método "ordenar casillas" para mezcla las casillas antes de elegir
+        return random.choice(self.__lista_de_casillas) #Elige y devuelve
+    
 
     
